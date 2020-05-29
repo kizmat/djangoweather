e0b4be792f7d7806286690215df8754a84a9bd73
Kizmat <kizmat@users.noreply.github.com>
2020-05-29 15:02:54 +0300

    add color coded aqi box, move aqi code from home.html to view.py

--- lookup/views.py
+++ lookup/views.py
@@ -14,9 +14,26 @@ def home(request):
 	except Exception as e:
 		api = "API Error...."
 
+	if api[0]['Category']['Name'] == "Good":
+  		category_description = "The AQI value for your community is between 0 and 50. Air quality is satisfactory and poses little or no health risk."
+  		category_color = "good"
+	elif api[0]['Category']['Name'] == "Moderate":
+		category_description = "The AQI is between 51 and 100. Air quality is acceptable; however, pollution in this range may pose a moderate health concern for a very small number of individuals. People who are unusually sensitive to ozone or particle pollution may experience respiratory symptoms."
+		category_color = "moderate"
+	elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+		category_description = "The AQI is between 51 and 100. Air quality is acceptable; however, pollution in this range may pose a moderate health concern for a very small number of individuals. People who are unusually sensitive to ozone or particle pollution may experience respiratory symptoms." 
+		category_color = "usg"
+	elif api[0]['Category']['Name'] == "Unhealthy":
+		category_description = "The AQI is between 51 and 100. Air quality is acceptable; however, pollution in this range may pose a moderate health concern for a very small number of individuals. People who are unusually sensitive to ozone or particle pollution may experience respiratory symptoms." 
+		category_color = "unhealthy"
+	elif api[0]['Category']['Name'] == "Very Unhealthy":
+		category_description = "The AQI is between 51 and 100. Air quality is acceptable; however, pollution in this range may pose a moderate health concern for a very small number of individuals. People who are unusually sensitive to ozone or particle pollution may experience respiratory symptoms."
+		category_color = "veryunhealthy"
+	elif api[0]['Category']['Name'] == "Hazardous":
+		category_description = "The AQI is between 51 and 100. Air quality is acceptable; however, pollution in this range may pose a moderate health concern for a very small number of individuals. People who are unusually sensitive to ozone or particle pollution may experience respiratory symptoms."
+		category_color = "hazardous"
 	
-	
-	return render(request, 'home.html', {'api': api})
+	return render(request, 'home.html', {'api': api, 'category_description': category_description, 'category_color': category_color})
 
 def about(request):
 	return render(request, 'about.html', {})
